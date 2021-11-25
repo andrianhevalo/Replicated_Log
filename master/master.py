a52029e360ef1ee3dcb0c7446876dc584834d85b
Iryna Pastukhova <>
2021-11-25 18:07:06 +0200

    Simplified master logic according to review comments

--- master/master.py
+++ master/master.py
@@ -35,36 +35,16 @@ async def append_message(message, w: int):
     return result[1]
 
 
-async def _post_to_endpoint_with_concern(endpoint, data, semaphore) -> (bool, json):
+async def _post_to_endpoint(endpoint, data) -> (bool, json):
     try:
         result = await requests.post(endpoint, json=data)
     except:
         print('Concern-replication exception')
         return False, data
     if result.status_code == 201:
-        if semaphore.locked():
-            print(f'semaphore is locked!')
-            return True, data
-        if await semaphore.acquire():
-            return True, data
-        else:
-            print('stopped!')
-            return True, data
+        return True, data
     else:
         return False, data
-
-
-async def _async_post_to_endpoint(endpoint, data) -> bool:
-    try:
-        result = await requests.post(endpoint, json=data)
-        print(f'status code {result.status_code}')
-    except:
-        print('Replication exception')
-        return False
-    if result.status_code == 201:
-        return True
-    else:
-        return False
 
 
 async def _replicate(message, w) -> (bool, str):
@@ -76,41 +56,33 @@ async def _replicate(message, w) -> (bool, str):
         message_list.append(message_to_send)
         tasks = []
 
-        if w == 1:
+        w = w - 1
+        if w > len(endpoints):
+            w = len(endpoints)
+
+        for endpoint in endpoints:
+            task = asyncio.create_task(_post_to_endpoint(endpoint, message_to_send))
+            tasks.append(task)
+
+        tasks_list = list(tasks)
+
+        if w == 0:
             _append_to_available_list(message_to_send)
-
-            for endpoint in endpoints:
-                task = asyncio.create_task(_async_post_to_endpoint(endpoint, message_to_send))
-                tasks.append(task)
-            asyncio.gather(*list(tasks))
+            asyncio.gather(*tasks_list)
             return True, 'Message was successfully saved'
-        else:
-            w = w - 1
-            if w > len(endpoints):
-                w = len(endpoints)
-            semaphore = asyncio.Semaphore(w)
 
-            for endpoint in endpoints:
-                task = asyncio.create_task(_post_to_endpoint_with_concern(endpoint, message_to_send, semaphore))
-                tasks.append(task)
-
-            # results = await asyncio.gather(*list(tasks))
-
-            tasks_list = list(tasks)
-            for cor in asyncio.as_completed(tasks_list):
-                result = await cor
-                if semaphore.locked():
-                    _append_to_available_list(result[1])
-                    return True, 'Message was successfully saved'
-            return False, 'Failed to save a message'
-            # for result in results:
-            #     if not result:
-            #         # message_list.remove(message_to_send)
-            #         return False, 'Failed to save a message'
-            # return True, 'Message was successfully saved'
+        for cor in asyncio.as_completed(tasks_list):
+            result = await cor
+            if result:
+                w -= 1
+                print(f'result = {result}, w = {w}')
+            if w <= 0:
+                print(f'w = {w}')
+                _append_to_available_list(result[1])
+                return True, 'Message was successfully saved'
+        return False, 'Failed to save a message'
 
 
-#TODO: think about adding lock for message lists
 def _append_to_available_list(message):
     if not available_message_list.__contains__(message):
         available_message_list.append(message)
